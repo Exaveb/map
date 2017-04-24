934697e9592d760cdf0588d13ab15ba07cbc6ed5
Exaveb <exaveb@gmail.com>
2017-04-24 11:09:17 +0300

    Add buttons "OK" and "Cancel". Add demo templates for input menu.

--- map.js
+++ map.js
@@ -2,7 +2,116 @@
  * Created by exaveb on 16.04.2017.
  */
 var Handlebars = require('handlebars');
-
+var test_context = {
+    id:"001",
+    years:[
+        {
+            title:"1990",
+            operations:[
+                {
+                    type:"one",
+                    action: "true"
+                },
+                {
+                    type:"two",
+                    action: "false"
+                },
+                {
+                    type:"three",
+                    action: "true"
+                },
+                {
+                    type:"four",
+                    action: "true"
+                }
+            ]
+        },
+        {
+            title:"1991",
+            operations:[
+                {
+                    type:"one",
+                    action: "true"
+                },
+                {
+                    type:"two",
+                    action: "false"
+                },
+                {
+                    type:"three",
+                    action: "true"
+                },
+                {
+                    type:"four",
+                    action: "true"
+                }
+            ]
+        },
+        {
+            title:"1992",
+            operations:[
+                {
+                    type:"one",
+                    action: "true"
+                },
+                {
+                    type:"two",
+                    action: "false"
+                },
+                {
+                    type:"three",
+                    action: "true"
+                },
+                {
+                    type:"four",
+                    action: "true"
+                }
+            ]
+        },
+        {
+            title:"1993",
+            operations:[
+                {
+                    type:"one",
+                    action: "true"
+                },
+                {
+                    type:"two",
+                    action: "false"
+                },
+                {
+                    type:"three",
+                    action: "true"
+                },
+                {
+                    type:"four",
+                    action: "true"
+                }
+            ]
+        },
+        {
+            title:"1994",
+            operations:[
+                {
+                    type:"one",
+                    action: "true"
+                },
+                {
+                    type:"two",
+                    action: "false"
+                },
+                {
+                    type:"three",
+                    action: "true"
+                },
+                {
+                    type:"four",
+                    action: "true"
+                }
+            ]
+        }
+    ]
+};
 
 var map;
 function initMap() {
@@ -14,116 +123,7 @@ function initMap() {
 
     });
 
-    var test_context = {
-        id:"001",
-        years:[
-            {
-                title:"1990",
-                operations:[
-                    {
-                        type:"one",
-                        action: "true"
-                    },
-                    {
-                        type:"two",
-                        action: "false"
-                    },
-                    {
-                        type:"three",
-                        action: "true"
-                    },
-                    {
-                        type:"four",
-                        action: "true"
-                    }
-                ]
-            },
-            {
-                title:"1991",
-                operations:[
-                    {
-                        type:"one",
-                        action: "true"
-                    },
-                    {
-                        type:"two",
-                        action: "false"
-                    },
-                    {
-                        type:"three",
-                        action: "true"
-                    },
-                    {
-                        type:"four",
-                        action: "true"
-                    }
-                ]
-            },
-            {
-                title:"1992",
-                operations:[
-                    {
-                        type:"one",
-                        action: "true"
-                    },
-                    {
-                        type:"two",
-                        action: "false"
-                    },
-                    {
-                        type:"three",
-                        action: "true"
-                    },
-                    {
-                        type:"four",
-                        action: "true"
-                    }
-                ]
-            },
-            {
-                title:"1993",
-                operations:[
-                    {
-                        type:"one",
-                        action: "true"
-                    },
-                    {
-                        type:"two",
-                        action: "false"
-                    },
-                    {
-                        type:"three",
-                        action: "true"
-                    },
-                    {
-                        type:"four",
-                        action: "true"
-                    }
-                ]
-            },
-            {
-                title:"1994",
-                operations:[
-                    {
-                        type:"one",
-                        action: "true"
-                    },
-                    {
-                        type:"two",
-                        action: "false"
-                    },
-                    {
-                        type:"three",
-                        action: "true"
-                    },
-                    {
-                        type:"four",
-                        action: "true"
-                    }
-                ]
-            }
-        ]
-    };
+
     var centerControlDiv = document.createElement('div');
     var menudiv = document.createElement('div');
     menudiv.id = 'menu';
@@ -143,6 +143,17 @@ function initMap() {
     centerControlDiv.innerHTML = html;
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
 
+    var ok_can_div = document.createElement('div');
+    ok_can_div.id = 'ok_can_div';
+    source = document.getElementById('two_btn').innerHTML;
+    template = Handlebars.compile(source);
+    html = template();
+    ok_can_div.innerHTML = html;
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(ok_can_div);
+    ok_can_div.style.display = 'none';
+
+
+
 
 
     google.maps.event.addListener(map,'click',function () {
@@ -153,7 +164,10 @@ function initMap() {
 
 
 function add_click() {
-    var controlUI = document.getElementById('btn');
+    var controlUI = document.getElementById('addbtn');
+    var ok_can_btn = document.getElementById('ok_can_div');
+    var ok = document.getElementById('ok_btn');
+    var cancel = document.getElementById('cancel_btn');
 
     var drawingManager = new google.maps.drawing.DrawingManager({
         drawingMode: google.maps.drawing.OverlayType.POLYGON,
@@ -163,14 +177,49 @@ function add_click() {
     drawingManager.setMap(map);
     console.log(map.controls);
     controlUI.style.display = "none";
+    ok_can_btn.style.display = 'block';
+    console.log(ok_can_btn);
     google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
         console.log(polygon);
         drawingManager.setMap(null);
-        controlUI.style.display = "block";
+        var menudiv = document.getElementById('menu');
+        var source   = document.getElementById("inputform").innerHTML,
+            template = Handlebars.compile(source),
+            html    = template();
+        menudiv.innerHTML = html;
+        menudiv.classList.add('show');
 
-        google.maps.event.addListener(polygon,'click',function () {
-            document.getElementById('menu').classList.add('show');
-            console.log( document.getElementById('menu').classList);
+        cancel.addEventListener('click',function(){
+                polygon.setMap(null);
+                polygon = null;
+            controlUI.style.display = "block";
+            ok_can_btn.style.display = 'none';
+            menudiv.classList.remove('show');
+
+            var source   = document.getElementById("info").innerHTML,
+                template = Handlebars.compile(source),
+                // context = {title: "My New Post", body: "This is my first post!"},
+                html    = template(test_context);
+            menudiv.innerHTML = html;
+        });
+
+       ok.addEventListener('click',function () {
+            controlUI.style.display = "block";
+            ok_can_btn.style.display = 'none';
+           menudiv.classList.remove('show');
+
+           var source   = document.getElementById("info").innerHTML,
+               template = Handlebars.compile(source),
+               // context = {title: "My New Post", body: "This is my first post!"},
+               html    = template(test_context);
+           menudiv.innerHTML = html;
+
+            google.maps.event.addListener(polygon, 'click', function () {
+                document.getElementById('menu').classList.add('show');
+                console.log(document.getElementById('menu').classList);
+            })
+
         })
     });
 }
+
